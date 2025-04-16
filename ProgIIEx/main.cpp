bfa5abf91c4da48dd22400d08387bb8f7677e8ab
oroji <vincenzobarbato05@gmail.com>
2025-04-16 17:01:26 +0200

    Initial Commit

---
+++ ProgIIEx/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ProgIIEx
+//
+//  Created by Vincenzo Barbato on 16/04/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
